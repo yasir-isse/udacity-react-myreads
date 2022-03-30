fec7850938ae93276081d7514eb32717cc4628df
Yasir <code.yasir@gmail.com>
2022-03-30 09:11:24 +0300

    add react router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOMClient from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./index.css";
 import App from "./App";
+import SearchPage from "./Components/SearchPage";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+const container = document.getElementById("root");
+const root = ReactDOMClient.createRoot(container);
+
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="search" element={<SearchPage />} />
+      </Route>
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>There's nothing here!</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>
 );
